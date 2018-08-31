06f173a9f18206cfd4d829f87c090a3c334d17f8
Monde Duinkharjav <budmonde@instagram.com>
2018-08-31 10:52:20 -0700

    Reformat codebase

--- src/app.js
+++ src/app.js
@@ -11,18 +11,18 @@ const app = express();
 const board = new Board(WORDS_LIST_FNAME, NUM_WORDS);
 
 app.get('/', function(req, res) {
-	res.send(board.board);
+  res.send(board.board);
 });
 
 app.get('/flip', function(req, res) {
-	const idx = parseInt(req.query.idx);
-	board.flipCard(idx);
-	res.send(board.board);
+  const idx = parseInt(req.query.idx);
+  board.flipCard(idx);
+  res.send(board.board);
 });
 
 const port = 3000;
 
 const server = http.Server(app);
 server.listen(port, function() {
-	console.log('Server running on port: ' + port);
+  console.log('Server running on port: ' + port);
 });
